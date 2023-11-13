597f7fe0eee7edfb743c04414d2b86b501cff283
DanielaNahir <danielanahir927@gmail.com>
2023-11-13 18:18:10 -0300

    interface base de datos

---
+++ Interfaces/IBaseDeDatos.cs
@@ -0,0 +1,12 @@
+﻿namespace Interfaces
+{
+    public interface IBaseDeDatos<T>
+    {
+        List<T> ObtenerTodos();
+        T ObtenerPorId(int id);
+        void Insertar(T entidad);
+        void Actualizar(T entidad);
+        void Eliminar(int id);
+    }
+}
+}
